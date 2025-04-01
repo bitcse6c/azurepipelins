e1cdd65b66008676113e2161c5064eb75b236fb8
Rohit-kb07 <rohitkumar070404@gmail.com>
2025-04-01 12:01:01 +0530

    initial commit

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App
+{
+public static void main(String args[])
+{
+int a=134;
+int b=1;
+System.out.println(a+b);
+}
+}
---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    
+    {
+    int a=9;
+    int b=6;
+        System.out.println("the sum is "+a+"+"+b+" is "+(a+b));
+    }
+}
